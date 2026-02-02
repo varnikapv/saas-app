6e4f3ba78f160212c59a00124916035cd221da96
Varnika <pvarnika05@gmail.com>
2026-02-02 18:42:11 +0530

    Implemented the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,32 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import { Bricolage_Grotesque } from "next/font/google";
+// import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+const bricolage = Bricolage_Grotesque({
+  variable: "--font-bricolage",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'SaaS App',
-  description: 'Your SaaS application',
-}
+  title: "Converso",
+  description: "Real-time AI Teaching Platform",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <body className={`${bricolage.variable} antialiased`}>
+       
+          <Navbar />
+          {children}
+        
       </body>
     </html>
-  )
+  );
 }
