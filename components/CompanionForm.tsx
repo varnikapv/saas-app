f01657d30a3c8192ca0bbd0bcca00268462296fa
Varnika <pvarnika05@gmail.com>
2026-02-08 09:34:42 +0530

    Companions section- search input and subject filter implemented

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -32,7 +32,7 @@ const formSchema = z.object({
     topic: z.string().min(1, { message: 'Topic is required.'}),
     voice: z.string().min(1, { message: 'Voice is required.'}),
     style: z.string().min(1, { message: 'Style is required.'}),
-    duration: z.number().min(1, { message: 'Duration is required.'}),
+    duration: z.coerce.number().min(1, { message: 'Duration is required.'}),
 })
 
 const CompanionForm = () => {
---
+++ components/SubjectFilter.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "./ui/select";
+import { subjects } from "@/constants";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+
+const SubjectFilter = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const currentSubject = searchParams.get("subject") || "all";
+
+    const handleSubjectChange = (value: string) => {
+        let newUrl = "";
+        if (value === "all") {
+            newUrl = removeKeysFromUrlQuery({
+                params: searchParams.toString(),
+                keysToRemove: ["subject"],
+            });
+        } else {
+            newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: "subject",
+                value: value,
+            });
+        }
+        router.push(newUrl, { scroll: false });
+    };
+
+    return (
+        <Select onValueChange={handleSubjectChange} value={currentSubject}>
+            <SelectTrigger className="input capitalize">
+                <SelectValue placeholder="Subject" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value="all">All subjects</SelectItem>
+                {subjects.map((subject) => (
+                    <SelectItem key={subject} value={subject} className="capitalize">
+                        {subject}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
+};
+
+export default SubjectFilter;
