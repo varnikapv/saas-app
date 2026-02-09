28a0a8e35aab1b47e62efd060ee7013df41cae83
Varnika <pvarnika05@gmail.com>
2026-02-09 23:59:08 +0530

    sentry implementation

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,12 @@ import CompanionCard from '@/components/CompanionCard';
 import CompanionsList from '@/components/CompanionsList';
 import CTA from '@/components/CTA';
 import { recentSessions } from '@/constants';
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 
 const Page = () => {
+    const companions = await getAllCompanions({limit: 3})
+    const recentSessionsCompanions = await getRecentSessions(10);
     return (
         <main>
         
@@ -11,40 +15,22 @@ const Page = () => {
                 Popular Companies
             </h1>
             <section className='home-section'> 
-                <CompanionCard 
-                id="123"
-                name="Neura the Brainy Explorer"
-                topic="Neural Network of the Brain"
-                subject = 'science'
-                duration = {45}
-                color="#ffda6e"
+                {companions.map((companion)=>(
+                    <CompanionCard 
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
 
                 />
-                <CompanionCard 
-                id="456"
-                name="Countsy the Number Wizard"
-                topic="Deriving Mathematical Fun"
-                subject = 'maths'
-                duration = {30}
-                color="#e5d0ff"
-
-                />
-                <CompanionCard 
-                id="789"
-                name="Verba the Language Sage"
-                topic="Unlocking Linguistic Mysteries"
-                subject = 'language'
-                duration = {30}
-                color="#BDE7FF"
-
-                />
+                ))}
+                
                 
 
             </section>
             <section className='home-section'>
                 <CompanionsList 
                 title = "Recently completed sessions" 
-                companions={recentSessions} 
+                companions={recentSessionsCompanions} 
                 className = "w-2/3 max-lg:w-full"  />
                 <CTA />
             </section>
